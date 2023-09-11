87270268b7f401a59a12205c8f83a4abad111624
NikolGraminova <nbackup619@gmail.com>
2023-09-11 18:37:38 +0200

    Finished with bonus.

---
+++ src/main/java/com/example/hellospringmvc/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.example.hellospringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        return "homepage";
+    }
+
+    @GetMapping("about-us")
+    public String aboutUs(){
+        return "about-us";
+    }
+
+    @GetMapping("terms-and-conditions")
+    public String termsAndConditions(){
+        return "terms-and-conditions";
+    }
+
+}
